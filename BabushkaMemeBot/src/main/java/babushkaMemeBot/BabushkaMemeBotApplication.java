68f675b7a348a647f7922b23659b08b2ce80b029
Maksym Volkov <maksym.volkov.99@gmail.com>
2019-07-13 21:18:44 +0200

    Changes in app class

--- BabushkaMemeBot/src/main/java/babushkaMemeBot/BabushkaMemeBotApplication.java
+++ BabushkaMemeBot/src/main/java/babushkaMemeBot/BabushkaMemeBotApplication.java
@@ -1,7 +1,5 @@
 package babushkaMemeBot;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
