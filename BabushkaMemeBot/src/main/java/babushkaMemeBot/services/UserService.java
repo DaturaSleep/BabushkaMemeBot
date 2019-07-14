e3baedee19742f69c162a09b1a00fe52b550c6f3
Maksym Volkov <maksym.volkov.99@gmail.com>
2019-07-14 19:25:39 +0200

    Login Function added to UserService

--- BabushkaMemeBot/src/main/java/babushkaMemeBot/services/UserService.java
+++ BabushkaMemeBot/src/main/java/babushkaMemeBot/services/UserService.java
@@ -1,5 +1,30 @@
 package babushkaMemeBot.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import babushkaMemeBot.dao.UserDAO;
+import babushkaMemeBot.models.User;
+
+@Service
 public class UserService {
 
+	@Autowired
+	UserDAO userRepository;
+	
+	@Transactional
+	public User loginUser(Long id, String username, String firstName, String lastName) {
+
+		User loggedUser;
+
+		if ((loggedUser = userRepository.getUserById(id)) == null) {
+			loggedUser = new User(id, username, firstName, lastName);
+			userRepository.saveUser(loggedUser);
+			return loggedUser;
+			
+		}
+		return loggedUser;
+		
+	}
 }
