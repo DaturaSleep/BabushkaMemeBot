2b0a35dd54c32e2ab8482ee145eac398d23b6587
Maksym Volkov <maksym.volkov.99@gmail.com>
2019-07-14 16:37:16 +0200

    Changed Bot Service to Bot Controller

--- BabushkaMemeBot/src/main/java/babushkaMemeBot/BabushkaMemeBotApplication.java
+++ BabushkaMemeBot/src/main/java/babushkaMemeBot/BabushkaMemeBotApplication.java
@@ -1,7 +1,5 @@
 package babushkaMemeBot;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,25 +7,25 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import babushkaMemeBot.services.TelegramBotService;
+import babushkaMemeBot.controllers.TelegramBotController;
 
 @ComponentScan(basePackages = "babushkaMemeBot.configuration")
 @Configuration
 public class BabushkaMemeBotApplication {
-	
+
 	public static void main(String args[]) {
 		ApiContextInitializer.init();
-		registerBot();
+		initBot();
 	}
 
-	private static void registerBot() {
+	private static void initBot() {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
 				BabushkaMemeBotApplication.class);
 
 		TelegramBotsApi botsApi = new TelegramBotsApi();
 
 		try {
-			botsApi.registerBot(context.getBean(TelegramBotService.class));
+			botsApi.registerBot(context.getBean(TelegramBotController.class));
 		} catch (TelegramApiException e) {
 			e.printStackTrace();
 		}
