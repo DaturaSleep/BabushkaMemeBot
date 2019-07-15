d5a20b11ae8be4805b9b66d9826be2cbec6ad0b8
Maksym Volkov <maksym.volkov.99@gmail.com>
2019-07-15 11:22:45 +0200

    MemeTemplate Dao implementation added

---
+++ BabushkaMemeBot/src/main/java/babushkaMemeBot/dao/impl/MemeTemplateDAOImpl.java
@@ -0,0 +1,67 @@
+package babushkaMemeBot.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import babushkaMemeBot.dao.MemeTemplateDAO;
+import babushkaMemeBot.models.MemeTemplate;
+
+public class MemeTemplateDAOImpl implements MemeTemplateDAO {
+
+	@Autowired
+	SessionFactory sessionFactory;
+
+	public List<MemeTemplate> getMemeTemplates() {
+
+		@SuppressWarnings("unchecked")
+		TypedQuery<MemeTemplate> typedQuery = sessionFactory.getCurrentSession().createQuery("From MemeTemplate");
+		return typedQuery.getResultList();
+	}
+
+	public MemeTemplate getMemeTemplate(Long id) {
+
+		@SuppressWarnings("unchecked")
+		TypedQuery<MemeTemplate> typedQuery = sessionFactory.getCurrentSession()
+				.createQuery("From MemeTemplate m where m.id =:id");
+		typedQuery.setParameter("id", id);
+
+		List<MemeTemplate> resultList = typedQuery.getResultList();
+
+		if (!resultList.isEmpty()) {
+			return resultList.get(0);
+		}
+
+		return null;
+	}
+
+	public void saveMemeTemplate(MemeTemplate memeTemplate) {
+		sessionFactory.getCurrentSession().save(memeTemplate);
+
+	}
+
+	public void deleteMemeTemplate(MemeTemplate memeTemplate) {
+		sessionFactory.getCurrentSession().delete(memeTemplate);
+
+	}
+
+	public void deleteMemeTemplate(Long id) {
+		@SuppressWarnings("unchecked")
+		TypedQuery<MemeTemplate> typedQuery = sessionFactory.getCurrentSession()
+				.createQuery("Delete From MemeTemplate m where m.id =:id");
+		typedQuery.setParameter("id", id);
+		typedQuery.executeUpdate();
+
+	}
+
+	public void deleteMemeTemplates() {
+		@SuppressWarnings("unchecked")
+		TypedQuery<MemeTemplate> typedQuery = sessionFactory.getCurrentSession()
+				.createQuery("Delete From MemeTemplate");
+		typedQuery.executeUpdate();
+	}
+
+}
