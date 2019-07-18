565a74d50ee89f5cb272ec94949cb05ba24b7645
Maksym Volkov <maksym.volkov.99@gmail.com>
2019-07-18 20:25:12 +0200

    Small changes with keyboard

--- BabushkaMemeBot/src/main/java/babushkaMemeBot/services/ScheduledService.java
+++ BabushkaMemeBot/src/main/java/babushkaMemeBot/services/ScheduledService.java
@@ -4,16 +4,15 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import babushkaMemeBot.dao.MemeTemplateDAO;
+import babushkaMemeBot.keyboard.Keyboard;
 import babushkaMemeBot.models.MemeTemplate;
 
 @Service
@@ -24,6 +23,12 @@ public class ScheduledService {
 
 	@Autowired
 	ObjectMapper jsonMapper;
+
+	@Autowired
+	Keyboard keyboard;
+
+	public ScheduledService() {
+	}
 
 	@Scheduled(cron = "0 4 */2 * * *")
 	@Transactional
@@ -40,6 +45,7 @@ public class ScheduledService {
 		if (refreshedMemeTemplateList.size() > 0) {
 			memeTemplateRepository.deleteMemeTemplates();
 			memeTemplateRepository.saveMemeTemplates(refreshedMemeTemplateList);
+			keyboard.refreshKeyboard(refreshedMemeTemplateList);
 		}
 	}
 
